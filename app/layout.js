22fce018d1db060bea2c8b146be62edadbd30a9e
Karolina <karolina.limanowska@gmail.com>
2024-09-24 23:32:27 +0200

    adding header and footer

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,8 @@
-import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import  { Inter } from 'next/font/google'
 
-const inter = Inter({
-  subsets: ['latin']
-});
+
 
 
 export const metadata = {
@@ -16,8 +12,14 @@ export const metadata = {
 
 export default function Layout({ children }) {
   return (
-    <html lang="en" className={inter.className}>
-      <body className={inter.className}>
+    <html lang="en">
+      <head>
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="font-inter">
         <Header />
          {children}
         <Footer /> 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,17 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        inter: ['Inter', 'sans-serif']
+        'inter': ['Inter', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif']
       },
+      colors: {
+        'zodiac': '#112F5F',
+        'seashell': '#F1F1F1',
+        'lightblue': '#F5F9FC',
+        'geyser': '#DEE2E6',
+        'codgray': '#151515',
+        'bostonblue': '##488AC6'
+      }
     },
   },
   plugins: [],

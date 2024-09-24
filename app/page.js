80772750628bded298001f2144db768c0585c26d
Karolina <karolina.limanowska@gmail.com>
2024-09-24 17:36:00 +0200

    working on a header

--- app/page.js
+++ app/page.js
@@ -8,33 +8,34 @@ import { ProductController } from '../controllers/productController.js';
 import React from "react";
 import ProductForm from "@/components/ProductForm";
 
+
 export default function Home() {
-	(async () =>{
-		let data = await UserController.test();		// Demo
-		console.log('bob', data);
-	})();
+	// (async () =>{
+	// 	let data = await UserController.test();		// Demo
+	// 	console.log('bob', data);
+	// })();
 
-	(async () =>{
-		console.log('ben', await ProductController.getAll());
-		console.log('bin', await ProductController.get(1));
-	})();
+	// (async () =>{
+	// 	console.log('ben', await ProductController.getAll());
+	// 	console.log('bin', await ProductController.get(1));
+	// })();
 	
-	// Users();
+	// // Users();
 
-	function testAddProduct(e){
-		e.preventDefault();
-		ProductController.add(new FormData(e.target));
-	}
+	// function testAddProduct(e){
+	// 	e.preventDefault();
+	// 	ProductController.add(new FormData(e.target));
+	// }
 
-	return(
-		<div>
-			<form onSubmit={(e) => testAddProduct(e)}>
-				<input type="text" name="test"/>
-				<input type="text" name="tester"/>
-				<input type="text" name="testest"/>
-				<input type="checkbox" name="testbox"/>
-				<button type="submit">test</button>
-			</form>
-		</div>
-	);
+	// return(
+	// 	<div>
+	// 		<form onSubmit={(e) => testAddProduct(e)}>
+	// 			<input type="text" name="test"/>
+	// 			<input type="text" name="tester"/>
+	// 			<input type="text" name="testest"/>
+	// 			<input type="checkbox" name="testbox"/>
+	// 			<button type="submit">test</button>
+	// 		</form>
+	// 	</div>
+	// );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,61 @@
+import Image from "next/image"
+
 export default function Header() {
     return (
-        <div>
-            HEADER
+        <div className="flex flex-row justify-between items-center w-full px-2 pt-3">
+            <div>
+                <Image 
+                    src='/images/ccbuild_logo.jpeg'
+                    width={180}
+                    height={100}
+                    alt='CCBuild logo'
+                />
+            </div> 
+            <div className="flex gap-10">
+                <div>
+                    <h2>CCBUILD</h2>
+                </div>
+                <div>
+                    <h2>TJÄNSTER</h2>
+                </div>
+                <div className="flex flex-row justify-between">
+                    <h2>MARKNADPLATSEN</h2>
+                    <Image 
+                        src='/icons/SVG.svg'
+                        width={16}
+                        height={16}
+                        alt="arrow"
+                    />
+                    
+                </div>
+                <div className="flex">
+                    <h2>PRODUKTBANKEN</h2>
+                    <Image 
+                        src='/icons/SVG.svg'
+                        width={16}
+                        height={16}
+                        alt="arrow"
+                    />
+                </div>
+                <div className="flex">
+                    <h2>USER</h2>
+                    <Image 
+                        src='/icons/SVG.svg'
+                        width={16}
+                        height={16}
+                        alt="arrow"
+                    />
+                </div>
+                <div className="flex">
+                    <h2>SV</h2>
+                    <Image 
+                        src='/icons/SVG.svg'
+                        width={16}
+                        height={16}
+                        alt="arrow"
+                    />
+                </div>
+            </div>
         </div>
-    )
+        )
 }
